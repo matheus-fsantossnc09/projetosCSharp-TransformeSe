ef1f9656eed995dbecc00448936e9ccef844a255
matheus-fsantossnc09 <matheus.fsantos72@senacsp.edu.br>
2025-03-28 17:20:27 -0300

    Fluxo de caixa

--- Controle de Fluxo/frmFluxoCaixa.Designer.cs
+++ Controle de Fluxo/frmFluxoCaixa.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFluxoCaixa));
             this.dtp_DataFolha = new System.Windows.Forms.DateTimePicker();
             this.lbl_Salario = new System.Windows.Forms.Label();
             this.txt_Salario = new System.Windows.Forms.TextBox();
@@ -42,14 +43,17 @@
             this.lbl_SalarioLiquido = new System.Windows.Forms.Label();
             this.lbl_DataFolha = new System.Windows.Forms.Label();
             this.lbl_Clubelazer = new System.Windows.Forms.Label();
+            this.btn_Limpar = new System.Windows.Forms.Button();
+            this.btn_Sair = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // dtp_DataFolha
             // 
             this.dtp_DataFolha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtp_DataFolha.Location = new System.Drawing.Point(43, 86);
+            this.dtp_DataFolha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtp_DataFolha.Location = new System.Drawing.Point(44, 86);
             this.dtp_DataFolha.Name = "dtp_DataFolha";
-            this.dtp_DataFolha.Size = new System.Drawing.Size(266, 22);
+            this.dtp_DataFolha.Size = new System.Drawing.Size(89, 22);
             this.dtp_DataFolha.TabIndex = 0;
             // 
             // lbl_Salario
@@ -68,7 +72,8 @@
             this.txt_Salario.Location = new System.Drawing.Point(43, 147);
             this.txt_Salario.Name = "txt_Salario";
             this.txt_Salario.Size = new System.Drawing.Size(219, 22);
-            this.txt_Salario.TabIndex = 2;
+            this.txt_Salario.TabIndex = 1;
+            this.txt_Salario.TextChanged += new System.EventHandler(this.txt_Salario_TextChanged);
             // 
             // ckb_PlanoSaude
             // 
@@ -80,28 +85,28 @@
             this.ckb_PlanoSaude.TabIndex = 3;
             this.ckb_PlanoSaude.Text = "Plano de Saúde";
             this.ckb_PlanoSaude.UseVisualStyleBackColor = true;
+            this.ckb_PlanoSaude.CheckedChanged += new System.EventHandler(this.ckb_PlanoSaude_CheckedChanged);
             // 
             // cbb_ClubeLazer
             // 
             this.cbb_ClubeLazer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbb_ClubeLazer.FormattingEnabled = true;
-            this.cbb_ClubeLazer.Items.AddRange(new object[] {
-            "Clube A",
-            "Clube B",
-            "Clube C"});
             this.cbb_ClubeLazer.Location = new System.Drawing.Point(43, 246);
             this.cbb_ClubeLazer.Name = "cbb_ClubeLazer";
             this.cbb_ClubeLazer.Size = new System.Drawing.Size(219, 24);
-            this.cbb_ClubeLazer.TabIndex = 4;
+            this.cbb_ClubeLazer.TabIndex = 2;
             // 
             // btn_Calcular
             // 
             this.btn_Calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Calcular.Location = new System.Drawing.Point(498, 12);
+            this.btn_Calcular.Image = ((System.Drawing.Image)(resources.GetObject("btn_Calcular.Image")));
+            this.btn_Calcular.Location = new System.Drawing.Point(522, 236);
             this.btn_Calcular.Name = "btn_Calcular";
             this.btn_Calcular.Size = new System.Drawing.Size(168, 43);
-            this.btn_Calcular.TabIndex = 5;
+            this.btn_Calcular.TabIndex = 6;
             this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Calcular.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btn_Calcular.UseVisualStyleBackColor = true;
             this.btn_Calcular.Click += new System.EventHandler(this.btn_Calcular_Click);
             // 
@@ -111,7 +116,7 @@
             this.txt_SalarioFolha.Location = new System.Drawing.Point(522, 107);
             this.txt_SalarioFolha.Name = "txt_SalarioFolha";
             this.txt_SalarioFolha.Size = new System.Drawing.Size(219, 22);
-            this.txt_SalarioFolha.TabIndex = 7;
+            this.txt_SalarioFolha.TabIndex = 3;
             // 
             // lbl_SalarioFolha
             // 
@@ -129,7 +134,7 @@
             this.txt_ImpostoRenda.Location = new System.Drawing.Point(522, 145);
             this.txt_ImpostoRenda.Name = "txt_ImpostoRenda";
             this.txt_ImpostoRenda.Size = new System.Drawing.Size(219, 22);
-            this.txt_ImpostoRenda.TabIndex = 9;
+            this.txt_ImpostoRenda.TabIndex = 4;
             // 
             // lbl_ImpostoRenda
             // 
@@ -147,7 +152,7 @@
             this.txt_SalarioLiquido.Location = new System.Drawing.Point(522, 184);
             this.txt_SalarioLiquido.Name = "txt_SalarioLiquido";
             this.txt_SalarioLiquido.Size = new System.Drawing.Size(219, 22);
-            this.txt_SalarioLiquido.TabIndex = 11;
+            this.txt_SalarioLiquido.TabIndex = 5;
             // 
             // lbl_SalarioLiquido
             // 
@@ -179,11 +184,41 @@
             this.lbl_Clubelazer.TabIndex = 13;
             this.lbl_Clubelazer.Text = "Clube de lazer";
             // 
+            // btn_Limpar
+            // 
+            this.btn_Limpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpar.Image = ((System.Drawing.Image)(resources.GetObject("btn_Limpar.Image")));
+            this.btn_Limpar.Location = new System.Drawing.Point(522, 301);
+            this.btn_Limpar.Name = "btn_Limpar";
+            this.btn_Limpar.Size = new System.Drawing.Size(168, 43);
+            this.btn_Limpar.TabIndex = 14;
+            this.btn_Limpar.Text = "Limpar";
+            this.btn_Limpar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Limpar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Limpar.UseVisualStyleBackColor = true;
+            this.btn_Limpar.Click += new System.EventHandler(this.btn_Limpar_Click);
+            // 
+            // btn_Sair
+            // 
+            this.btn_Sair.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Sair.Image = ((System.Drawing.Image)(resources.GetObject("btn_Sair.Image")));
+            this.btn_Sair.Location = new System.Drawing.Point(522, 364);
+            this.btn_Sair.Name = "btn_Sair";
+            this.btn_Sair.Size = new System.Drawing.Size(168, 43);
+            this.btn_Sair.TabIndex = 15;
+            this.btn_Sair.Text = "Sair";
+            this.btn_Sair.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Sair.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Sair.UseVisualStyleBackColor = true;
+            this.btn_Sair.Click += new System.EventHandler(this.btn_Sair_Click);
+            // 
             // frmFluxoCaixa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Sair);
+            this.Controls.Add(this.btn_Limpar);
             this.Controls.Add(this.lbl_Clubelazer);
             this.Controls.Add(this.lbl_DataFolha);
             this.Controls.Add(this.txt_SalarioLiquido);
@@ -200,6 +235,7 @@
             this.Controls.Add(this.dtp_DataFolha);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "frmFluxoCaixa";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -225,5 +261,7 @@
         private System.Windows.Forms.Label lbl_SalarioLiquido;
         private System.Windows.Forms.Label lbl_DataFolha;
         private System.Windows.Forms.Label lbl_Clubelazer;
+        private System.Windows.Forms.Button btn_Limpar;
+        private System.Windows.Forms.Button btn_Sair;
     }
 }
--- Controle de Fluxo/frmFluxoCaixa.cs
+++ Controle de Fluxo/frmFluxoCaixa.cs
@@ -16,32 +16,137 @@ namespace Controle_de_Fluxo
         public frmFluxoCaixa()
         {
             InitializeComponent();
+            desabilitarCampos();
+            carregaCombo();
         }
 
-        public void salarioFolha()
-        {
-            double salario, planoSaude = 200, resultadoCom = 0, resultadoSem;
+        public double salarioFolha = 0, impostoRenda = 0, salarioliquido = 0;
 
-            salario = Convert.ToDouble(txt_Salario.Text);
-
-            if (ckb_PlanoSaude.Checked)
-            {
-                resultadoCom = salario - planoSaude;
-                txt_SalarioFolha.Text = Convert.ToString(resultadoCom);
-            }
-            else
-            {
-                resultadoSem = salario;
-                txt_SalarioFolha.Text = Convert.ToString(resultadoSem);
-            }
-
-           
-            
-        }
 
         private void btn_Calcular_Click(object sender, EventArgs e)
         {
-            salarioFolha();
+            double salarioFolha = 0, impostoRenda = 0, salarioliquido = 0;
+            try
+            {
+
+                salarioFolha = Convert.ToDouble(txt_SalarioFolha.Text);
+
+                salarioliquido = salarioFolha - impostoRenda;
+
+                if (salarioFolha < 2259.20)
+                {
+                  txt_SalarioLiquido.Text = txt_SalarioFolha.Text;
+                }
+
+                else if (salarioFolha >= 2259.21 && salarioFolha <= 2826.65)
+                {
+                    impostoRenda = salarioFolha * 7.5 / 100;
+                    salarioliquido = (salarioFolha - (impostoRenda));
+
+                }
+
+
+                else if (salarioFolha >= 2826.66 && salarioFolha <= 3751.05)
+                {
+                    impostoRenda = salarioFolha * 15 / 100;
+                    salarioliquido = (salarioFolha - (impostoRenda));
+
+                }
+
+
+                else if (salarioFolha >= 3751.06 && salarioFolha <= 4664.68)
+                {
+                    impostoRenda = salarioFolha * 22.5 / 100;
+                    salarioliquido = (salarioFolha - (impostoRenda));
+
+                }
+
+
+                else if (salarioFolha > 4664.69)
+
+                {
+                    impostoRenda = salarioFolha * 27.5 / 100;
+                    salarioliquido = (salarioFolha - (impostoRenda));
+
+                }
+
+                if (ckb_PlanoSaude.Checked)
+                {
+                    salarioliquido = salarioliquido - 200;
+
+                }
+                if (cbb_ClubeLazer.SelectedIndex == 0)
+                {
+                    salarioliquido = salarioliquido - 100;
+                }
+                if (cbb_ClubeLazer.SelectedIndex == 1)
+                {
+                    salarioliquido = salarioliquido - 50;
+                }
+                if (cbb_ClubeLazer.SelectedIndex == 2)
+                {
+                    salarioliquido = salarioliquido - 30;
+                }
+
+
+                txt_ImpostoRenda.Text = Convert.ToString(impostoRenda);
+                txt_SalarioLiquido.Text = Convert.ToString(salarioliquido);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Inserir o salário",
+                    "Messagem do sistema",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error,
+                    MessageBoxDefaultButton.Button1);
+                limparCampos();
+            }
+        }
+
+        private void btn_Sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_Limpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+        public void limparCampos()
+        {
+            txt_Salario.Clear();
+            ckb_PlanoSaude.Checked = false;
+            cbb_ClubeLazer.Text = "";
+            txt_SalarioFolha.Clear();
+            txt_ImpostoRenda.Clear();
+            txt_SalarioLiquido.Clear();
+
+            txt_Salario.Focus();
+        }
+        public void desabilitarCampos()
+        {
+            txt_SalarioFolha.Enabled = false;
+            txt_ImpostoRenda.Enabled = false;
+            txt_SalarioLiquido.Enabled = false;
+        }
+        public void carregaCombo()
+        {
+            cbb_ClubeLazer.Items.Add("Clube A");
+            cbb_ClubeLazer.Items.Add("Clube B");
+            cbb_ClubeLazer.Items.Add("Clube C");
+        }
+
+        private void txt_Salario_TextChanged(object sender, EventArgs e)
+        {
+            txt_SalarioFolha.Text = txt_Salario.Text;
+        }
+
+
+        private void ckb_PlanoSaude_CheckedChanged(object sender, EventArgs e)
+        {
+
+
+
         }
     }
 }
